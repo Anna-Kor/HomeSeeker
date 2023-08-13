997a042ab4a2cb754b6aca4d9fcfb452bb540469
Anna-Kor <61621691+Anna-Kor@users.noreply.github.com>
2023-08-13 22:42:36 +0200

    refactor: change parameters passed to unit tests

--- HomeSeeker.API/Commands/UserCommands/RegisterUserCommand.cs
+++ HomeSeeker.API/Commands/UserCommands/RegisterUserCommand.cs
@@ -12,9 +12,17 @@ namespace HomeSeeker.API.Commands.UserCommands
         public string? LastName { get; set; }
 
         [Required]
-        public string? Username { get; set; }
+        public string Username { get; set; }
 
         [Required]
-        public string? Password { get; set; }
+        public string Password { get; set; }
+
+        public RegisterUserCommand(string firstName, string lastName, string username, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Username = username;
+            Password = password;
+        }
     }
 }
--- HomeSeeker.Tests/UsersTests/AuthenticationTest.cs
+++ HomeSeeker.Tests/UsersTests/AuthenticationTest.cs
@@ -11,12 +11,6 @@ namespace HomeSeeker.Tests.UsersTests
 {
     public class AuthenticationTest
     {
-        public static IEnumerable<object[]> Data =>
-          new List<object[]>
-          {
-                new object[] { new AuthenticateQuery("user", "password") }
-          };
-
         private readonly IUserRepository _userRepositoryMock;
         private readonly IJwtUtils _jwtUtils;
         private readonly IPasswordHelper _passwordHelperMock;
@@ -29,9 +23,11 @@ namespace HomeSeeker.Tests.UsersTests
         }
 
         [Theory]
-        [MemberData(nameof(Data))]
-        public async void AuthencticateUser_WhereUsernameAndPasswordIsCorrect_ShouldReturnOk(AuthenticateQuery query)
+        [InlineData("user", "password")]
+        public async void AuthencticateUser_WhereUsernameAndPasswordIsCorrect_ShouldReturnOk(string username, string password)
         {
+            var query = new AuthenticateQuery(username, password);
+
             _passwordHelperMock.VerifyPassword(default, default, default).ReturnsForAnyArgs(true);
             _userRepositoryMock.GetAll(default).ReturnsForAnyArgs(new List<UserModel> { new UserModel { Username = query.Username, Password = query.Password } });
             
@@ -43,9 +39,11 @@ namespace HomeSeeker.Tests.UsersTests
         }
 
         [Theory]
-        [MemberData(nameof(Data))]
-        public async void AuthencticateUser_WhereThereIsNotAnyUserInDb_ShouldReturnNull(AuthenticateQuery query)
-        {  
+        [InlineData("user", "password")]
+        public async void AuthencticateUser_WhereThereIsNotAnyUserInDb_ShouldReturnNull(string username, string password)
+        {
+            var query = new AuthenticateQuery(username, password);
+
             var handler = new UsersQueryHandler(_userRepositoryMock, _jwtUtils, _passwordHelperMock);
 
             var result = await handler.Handle(query, default);
@@ -54,11 +52,14 @@ namespace HomeSeeker.Tests.UsersTests
         }
 
         [Theory]
-        [MemberData(nameof(Data))]
-        public async void AuthencticateUser_WhereUsernameIsNotCorrect_ShouldReturnNull(AuthenticateQuery query)
+        [InlineData("user", "password")]
+        [InlineData(null, "password")]
+        public async void AuthencticateUser_WhereUsernameIsNotCorrect_ShouldReturnNull(string username, string password)
         {
+            var query = new AuthenticateQuery(username, password);
+
             _passwordHelperMock.VerifyPassword(default, default, default).ReturnsForAnyArgs(true);
-            _userRepositoryMock.GetAll(default).ReturnsForAnyArgs(new List<UserModel> { new UserModel { Username = query.Username + "somethingelse", Password = query.Password } });
+            _userRepositoryMock.GetAll(default).ReturnsForAnyArgs(new List<UserModel> { new UserModel { Username = "somethingelse", Password = query.Password } });
 
             var handler = new UsersQueryHandler(_userRepositoryMock, _jwtUtils, _passwordHelperMock);
 
@@ -68,9 +69,12 @@ namespace HomeSeeker.Tests.UsersTests
         }
 
         [Theory]
-        [MemberData(nameof(Data))]
-        public async void AuthencticateUser_WherePasswordIsNotCorrect_ShouldReturnNull(AuthenticateQuery query)
+        [InlineData("user", "password")]
+        [InlineData("user", null)]
+        public async void AuthencticateUser_WherePasswordIsNotCorrect_ShouldReturnNull(string username, string password)
         {
+            var query = new AuthenticateQuery(username, password);
+
             _passwordHelperMock.VerifyPassword(default, default, default).ReturnsForAnyArgs(false);
             _userRepositoryMock.GetAll(default).ReturnsForAnyArgs(new List<UserModel> { new UserModel { Username = query.Username, Password = query.Password } });
 
