31647b0f3774e256592574edefd4588b7fadd4cf
Anna-Kor <61621691+Anna-Kor@users.noreply.github.com>
2023-09-10 20:43:11 +0200

    refactor: move fetching to stores, type fixes

--- HomeSeeker.Page/src/stores/announcements.store.ts
+++ HomeSeeker.Page/src/stores/announcements.store.ts
@@ -1,14 +1,15 @@
-import { defineStore } from 'pinia';
+import { defineStore, storeToRefs } from 'pinia';
 import { type IHomeModel, HomesClient } from '@/clients';
 import { getErrorMessage } from '@/helpers';
-import { useAlertStore } from '@/stores';
+import { useAlertStore, useAuthStore } from '@/stores';
 
 const baseUrl = `${import.meta.env.VITE_API_URL}`;
 const client = new HomesClient(baseUrl);
 
 interface IAnnouncementsState {
+    home: IHomeModel | null,
     homes: IHomeModel[] | null,
-    maxPrice: number | null
+    maxPrice: number | undefined
 }
 
 export interface IGetActiveHomesQuery {
@@ -22,24 +23,49 @@ export const useAnnouncementsStore = defineStore({
     id: 'announcements',
     state: (): IAnnouncementsState => {
         return {
+            home: null,
             homes: null,
-            maxPrice: null
+            maxPrice: undefined
         }
     },
     actions: {
         async getMaxPrice() {
+            const alertStore = useAlertStore();
             try {
                 this.maxPrice = await client.getMaxPrice();
             } catch (error) {
-                const alertStore = useAlertStore();
                 alertStore.error(getErrorMessage(error));
             }
         },
         async getActive(query: IGetActiveHomesQuery) {
+            const alertStore = useAlertStore();
             try {
-                this.homes = await client.getActive(query.name, query.priceFrom, query.priceTo, query.city);
+                this.homes = await client.getActive(query.name, query.priceFrom, query.priceTo, query.city) as IHomeModel[];
             } catch (error) {
-                const alertStore = useAlertStore();
+                alertStore.error(getErrorMessage(error));
+            }
+        },
+        async getById(id: number) {
+            const alertStore = useAlertStore();
+
+            const authStore = useAuthStore();
+            const { user } = storeToRefs(authStore);
+            try {
+                client.setAuthToken(user.value?.token || undefined);
+                this.home = await client.getById(id) as IHomeModel;
+            } catch (error) {
+                alertStore.error(getErrorMessage(error));
+            }
+        },
+        async getByUser() {
+            const alertStore = useAlertStore();
+
+            const authStore = useAuthStore();
+            const { user } = storeToRefs(authStore);
+            try {
+                client.setAuthToken(user.value?.token || undefined);
+                this.homes = await client.getByUser() as IHomeModel[];
+            } catch (error) {
                 alertStore.error(getErrorMessage(error));
             }
         }
